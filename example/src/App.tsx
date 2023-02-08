468d6a194ead22f3634ddb7cc602f9307c3ec5c2
Yuri Logatto Pamplona <yuri@flourishfi.com>
2023-02-08 19:29:01 -0300

    implementing example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import api from './api';
+import { Flourish } from 'flourish-web-sdk-react';
 
 function App() {
+
+  const [ accessToken, setAccessToken ] = useState('')
+
+  useEffect(() => {
+      api.get('flourish-access-token').then(response => {
+          setAccessToken(response.data.flourishAccessToken)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flourish token={accessToken}
+              environment='staging'
+              language='en'
+              eventCallback={printEventData} />
   );
 }
+
+const printEventData = (data: string): void => {
+  console.log('Event Client side', data);
+};
 
 export default App;
